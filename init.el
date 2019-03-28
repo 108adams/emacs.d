c4ae45bc38261a0544dd3199ecec0683e69c80e6
Adam Kucharczyk <108adams@gmail.com>
2019-03-28 22:25:05 +0100

    updated config, python added

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Main init.el file for Emacs
+;;; init.el ---  Main init.el file for Emacs
 
 ;;; Commentary:
 ;; This file allows Emacs to initialize my customizations
@@ -8,8 +8,8 @@
 ;; Configure package.el to include MELPA.
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
 ;; Ensure that use-package is installed.
@@ -21,20 +21,37 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Always compile packages, and use the newest version available.
+(use-package auto-compile
+  :config (auto-compile-on-load-mode))
+(setq load-prefer-newer t)
+
+;; start machines for literate programming
 (use-package org)
 (org-babel-load-file "~/.emacs.d/configuration.org")
-;;; init.el ends here
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("04232a0bfc50eac64c12471607090ecac9d7fd2d79e388f8543d1c5439ed81f5" default)))
  '(package-selected-packages
    (quote
-    (wgrep smex flx counsel yasnippet dired-hide-dotfiles deft markdown-mode graphviz-dot-mode gnuplot yaml-mode flycheck-package rainbow-delimiters paredit web-mode use-package undo-tree projectile magit js2-mode flycheck company))))
+    (ob-ipython zenburn-theme counsel deft yasnippet yaml-mode which-key wgrep web-mode use-package undo-tree shut-up rainbow-delimiters paredit paradox org markdown-mode+ magit loop json-mode js2-mode graphviz-dot-mode gnuplot flycheck-package dired-hide-dotfiles diff-hl company auto-compile ag)))
+ '(undo-tree-visualizer-diff t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(load-theme 'zenburn t)
+
+;;; init.el ends here
